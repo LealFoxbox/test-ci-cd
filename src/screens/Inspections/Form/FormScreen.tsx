7fa732b3533f76854224deb557c1a460f2a3b06a
diegofoxbox <diego@foxbox.com>
2021-02-05 17:06:12 -0300

    fix: styling and other issues

--- src/screens/Inspections/Form/FormScreen.tsx
+++ src/screens/Inspections/Form/FormScreen.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, ListRenderItem, View } from 'react-native';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { Button, Text, useTheme } from 'react-native-paper';
 import { TextInputProps } from 'react-native-paper/lib/typescript/src/components/TextInput/TextInput';
-import { Formik } from 'formik';
+import { Formik, FormikProps } from 'formik';
 import { fromPairs, memoize, set } from 'lodash/fp';
 
 import ExpandedGallery from 'src/components/ExpandedGallery';
@@ -12,7 +12,7 @@ import { PersistentUserStore } from 'src/pullstate/persistentStore';
 import { updateDraftFieldsAction } from 'src/pullstate/actions';
 import { INSPECTIONS_FORM } from 'src/navigation/screenNames';
 import { InspectionsNavigatorParamList } from 'src/navigation/InspectionsNavigator';
-import { DraftForm, DraftPhoto } from 'src/types';
+import { DraftField, DraftForm, DraftPhoto, Rating } from 'src/types';
 
 import TextCard from './TextCard';
 import NumberCard from './NumberCard';
@@ -20,6 +20,98 @@ import NumberCard from './NumberCard';
 const getInitialValues = memoize((draft: DraftForm) => {
   return fromPairs(draft.fields.map((field) => [field.formFieldId, field]));
 });
+
+const createRenderCard = (
+  { values, setFieldValue }: FormikProps<Record<string, DraftField>>,
+  {
+    setExpandedPhoto,
+    assignmentId,
+    ratings,
+    theme,
+  }: {
+    setExpandedPhoto: React.Dispatch<React.SetStateAction<{ photos: string[]; index: number }>>;
+    assignmentId: number;
+    ratings: Record<string, Rating>;
+    theme: ReactNativePaper.Theme;
+  },
+): ListRenderItem<DraftField> => {
+  return ({ item: draftField }) => {
+    const fieldValue = values[draftField.formFieldId];
+    const rating = ratings[fieldValue.ratingTypeId];
+
+    const handleBlur = () => updateDraftFieldsAction(assignmentId, values);
+    const handleTapPhoto = (index: number) => setExpandedPhoto({ index, photos: draftField.photos.map((p) => p.uri) });
+    const handleTakePhoto = (uri: string, isFromGallery: boolean) => {
+      const newPhoto: DraftPhoto = {
+        isFromGallery,
+        uri,
+        latitude: null, // Latitude where the inspection was started or first available location coordinates
+        longitude: null, // Longitude where the inspection was started or first available location coordinates
+        created_at: Date.now(), // timestamp in format "2020-01-08T14:52:56-07:00",
+      };
+
+      setFieldValue(`${fieldValue.formFieldId}`, set('photos', fieldValue.photos.concat([newPhoto]), fieldValue));
+
+      const newValues = set(`${draftField.formFieldId}.photos`, fieldValue.photos.concat([newPhoto]), values);
+      updateDraftFieldsAction(assignmentId, newValues);
+    };
+
+    const commentInputProps: TextInputProps = {
+      value: fieldValue.comment || '',
+      onChangeText: (value) => {
+        setFieldValue(`${fieldValue.formFieldId}`, { ...fieldValue, comment: value });
+      },
+      onBlur: handleBlur,
+      placeholder: 'Add a comment...',
+      theme,
+    };
+
+    if (fieldValue.ratingTypeId === 6) {
+      // NumberCard
+      const numberInputProps: TextInputProps = {
+        value: fieldValue.number_choice || '',
+        onChangeText: (value) => {
+          setFieldValue(`${fieldValue.formFieldId}`, {
+            ...fieldValue,
+            number_choice: value,
+          });
+        },
+        onBlur: handleBlur,
+        label: fieldValue.name,
+        theme,
+      };
+
+      return (
+        <NumberCard
+          id={fieldValue.formFieldId}
+          key={fieldValue.formFieldId}
+          rating={rating}
+          name={fieldValue.name}
+          description={fieldValue.description}
+          commentInputProps={commentInputProps}
+          numberInputProps={numberInputProps}
+          photos={fieldValue.photos}
+          onTapPhoto={handleTapPhoto}
+          onTakePhoto={handleTakePhoto}
+        />
+      );
+    }
+
+    // TextCard
+    return (
+      <TextCard
+        id={fieldValue.formFieldId}
+        key={fieldValue.formFieldId}
+        name={fieldValue.name}
+        description={fieldValue.description}
+        commentInputProps={commentInputProps}
+        photos={fieldValue.photos}
+        onTapPhoto={handleTapPhoto}
+        onTakePhoto={handleTakePhoto}
+      />
+    );
+  };
+};
 
 const EditFormScreen: React.FC<{}> = () => {
   const {
@@ -63,109 +155,30 @@ const EditFormScreen: React.FC<{}> = () => {
         onClose={() => setExpandedPhoto((s) => ({ ...s, index: -1 }))}
         renderFooter={(_currentImage: any) => (
           <View>
-            <Text>Trash this</Text>
+            <Text>.</Text>
           </View>
         )}
       />
       <Formik initialValues={initialValues} onSubmit={submit}>
-        {({ values, setFieldValue, handleSubmit }) => (
+        {(formikProps) => (
           <FlatList
             contentContainerStyle={{
               justifyContent: 'flex-start',
             }}
-            ListHeaderComponent={() => (draft.notes ? <Notes value={draft.notes} isCard /> : null)}
+            ListHeaderComponent={<Notes value={draft.notes} isCard />}
             ListFooterComponent={() => (
               <Button
-                onPress={handleSubmit}
+                onPress={formikProps.handleSubmit}
                 mode="contained"
                 dark
-                style={{ width: 120, alignSelf: 'flex-end', marginTop: 10 }}
+                style={{ width: 120, alignSelf: 'flex-end', marginTop: 10, marginBottom: 20, marginHorizontal: 10 }}
               >
                 Submit
               </Button>
             )}
             data={draft.fields}
             keyExtractor={(item) => `${item.formFieldId}`}
-            renderItem={({ item: draftField }) => {
-              const fieldValue = values[draftField.formFieldId];
-              const rating = ratings[fieldValue.ratingTypeId];
-
-              const handleBlur = () => updateDraftFieldsAction(assignmentId, values);
-              const handleTapPhoto = (index: number) =>
-                setExpandedPhoto({ index, photos: draftField.photos.map((p) => p.uri) });
-              const handleTakePhoto = (uri: string, isFromGallery: boolean) => {
-                const newPhoto: DraftPhoto = {
-                  isFromGallery,
-                  uri,
-                  latitude: null, // Latitude where the inspection was started or first available location coordinates
-                  longitude: null, // Longitude where the inspection was started or first available location coordinates
-                  created_at: Date.now(), // timestamp in format "2020-01-08T14:52:56-07:00",
-                };
-
-                setFieldValue(
-                  `${fieldValue.formFieldId}`,
-                  set('photos', fieldValue.photos.concat([newPhoto]), fieldValue),
-                );
-
-                const newValues = set(`${draftField.formFieldId}.photos`, fieldValue.photos.concat([newPhoto]), values);
-                updateDraftFieldsAction(assignmentId, newValues);
-              };
-
-              const commentInputProps: TextInputProps = {
-                value: fieldValue.comment || '',
-                onChangeText: (value) => {
-                  setFieldValue(`${fieldValue.formFieldId}`, { ...fieldValue, comment: value });
-                },
-                onBlur: handleBlur,
-                placeholder: 'Add a comment...',
-                theme,
-              };
-
-              if (fieldValue.ratingTypeId === 6) {
-                // NumberCard
-                const numberInputProps: TextInputProps = {
-                  value: fieldValue.number_choice || '',
-                  onChangeText: (value) => {
-                    setFieldValue(`${fieldValue.formFieldId}`, {
-                      ...fieldValue,
-                      number_choice: value,
-                    });
-                  },
-                  onBlur: handleBlur,
-                  label: fieldValue.name,
-                  theme,
-                };
-
-                return (
-                  <NumberCard
-                    id={fieldValue.formFieldId}
-                    key={fieldValue.formFieldId}
-                    rating={rating}
-                    name={fieldValue.name}
-                    description={fieldValue.description}
-                    commentInputProps={commentInputProps}
-                    numberInputProps={numberInputProps}
-                    photos={fieldValue.photos}
-                    onTapPhoto={handleTapPhoto}
-                    onTakePhoto={handleTakePhoto}
-                  />
-                );
-              }
-
-              // TextCard
-              return (
-                <TextCard
-                  id={fieldValue.formFieldId}
-                  key={fieldValue.formFieldId}
-                  name={fieldValue.name}
-                  description={fieldValue.description}
-                  commentInputProps={commentInputProps}
-                  photos={fieldValue.photos}
-                  onTapPhoto={handleTapPhoto}
-                  onTakePhoto={handleTakePhoto}
-                />
-              );
-            }}
+            renderItem={createRenderCard(formikProps, { setExpandedPhoto, assignmentId, ratings, theme })}
           />
         )}
       </Formik>
