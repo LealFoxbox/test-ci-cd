36fe63d94be7fe4be0a331cd11495cefcd17d9c4
rodrigovive <cherry199662@gmail.com>
2021-05-24 19:54:37 -0500

    fix: prebuild

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 echo "Installing tools and setting env variables"
 
-gem install cocoapods-user-defined-build-types
-
-(echo $ENV_CONTENT | base64 --decode) > .env
-
 BUILD_VERSION="com.orangeqc.app"
 VERSION_NUMBER=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
-
-if [ "$APP_ENV" == "staging" ]; then
-  BUILD_VERSION="$BUILD_VERSION.staging"
-fi;
 
 RELEASE_VERSION="$BUILD_VERSION@$VERSION_NUMBER+$APPCENTER_BUILD_ID"
 printf "\nSetting RELEASE_VERSION to .env: $RELEASE_VERSION"
