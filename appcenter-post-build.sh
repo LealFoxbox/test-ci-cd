4d71affdd717771b9c1fa66ebfec036657c420b3
rodrigovive <cherry199662@gmail.com>
2021-06-25 16:26:27 -0500

    fix: correct sourcemaps on appcenter

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,12 +4,12 @@ if [ "$AGENT_JOBSTATUS" = "Succeeded" ];  then
   cat $APPCENTER_SOURCE_DIRECTORY/.env
   eval $(grep -v '^#' $APPCENTER_SOURCE_DIRECTORY/.env | sed 's/^/export /')
   # end exporting
-  npx react-native bundle --entry-file index.js --platform $APPCENTER_CURRENT_PLATFORM --dev false --bundle-output $APPCENTER_OUTPUT_DIRECTORY/index.main.bundle --sourcemap-output $APPCENTER_OUTPUT_DIRECTORY/index.main.bundle.map
+  npx react-native bundle --entry-file index.js --platform $APPCENTER_CURRENT_PLATFORM --dev false --bundle-output $APPCENTER_OUTPUT_DIRECTORY/index.android.bundle --sourcemap-output $APPCENTER_OUTPUT_DIRECTORY/index.android.bundle.map
   printf "\nCreating a Sentry release with id $RELEASE_VERSION"
   # Create a release
    yarn sentry-cli releases new -p "$SENTRY_PROJECT" "$RELEASE_VERSION"
    yarn sentry-cli releases set-commits --auto "$RELEASE_VERSION" --log-level debug
-   yarn sentry-cli releases files $RELEASE_VERSION upload-sourcemaps $APPCENTER_OUTPUT_DIRECTORY/index.main.bundle* --dist $APPCENTER_BUILD_ID --rewrite --strip-common-prefix
+   yarn sentry-cli releases files $RELEASE_VERSION upload-sourcemaps $APPCENTER_OUTPUT_DIRECTORY/index.android.bundle* --dist $APPCENTER_BUILD_ID --rewrite --strip-common-prefix
    yarn sentry-cli releases finalize "$RELEASE_VERSION"
    yarn sentry-cli releases deploys "$RELEASE_VERSION" new -e "$APP_ENV"
    echo "Sentry release created"
