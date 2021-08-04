5de2a5646fb93549f35cf91382ed21560fbcef79
rodrigo_viveros <cherry199662@gmail.com>
2021-08-03 23:59:47 -0500

    feat: in app review

--- android/app/src/main/java/com/orangeqc/app/MainApplication.java
+++ android/app/src/main/java/com/orangeqc/app/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.ibits.react_native_in_app_review.AppReviewPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
