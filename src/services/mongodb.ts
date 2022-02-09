b63075818368adb65a685f5204cdfa736a03b30b
drfoxbox <94710514+drfoxbox@users.noreply.github.com>
2022-02-09 15:52:20 -0300

    Fix/mongodb storage (#74)
    * fix: changed the way it was persisting the data into Database
    
    * fix: mongodb improvements - shouldInsert
    
    * fix: mongodb improvements - get data from database
    
    * fix: mongodb improvements - replace replaceAll for replace(/\n/g, ',')
    
    * fix: mongodb improvements - clear AsyncStorage
    
    * fix: mongodb improvements - Sentry
    
    * fix: mongodb improvements - Type useUnknownInCatchVariables
    
    * fix: bump version
    
    * fix: mongodb improvements - changes on search function
    
    Co-authored-by: Diego Almeida <diego.ramos@foxbox.coml>

--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -2,9 +2,11 @@
 import Datastore from 'react-native-local-mongodb';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { escapeRegExp, filter, find, uniqBy, words } from 'lodash/fp';
+import * as Sentry from '@sentry/react-native';
 
 import config, { getMockFlags } from 'src/config';
 import { Assignment, Structure } from 'src/types';
+import { logErrorToSentry } from 'src/utils/logger';
 
 type StructuresDb = ReturnType<typeof createStructureDb>;
 type AssignmentDb = ReturnType<typeof createAssignmentDb>;
@@ -117,8 +119,126 @@ export function createAssignmentMock() {
   };
 }
 
+let shouldInsert = false;
+let structureKey = 1;
+let previous = '';
+let currentKey = `${config.APP_NAME}_structures`;
+
+const asyncMethods = {
+  getItem: async (key: string, callback?: (error?: Error, result?: string) => void): Promise<string | null> => {
+    return AsyncStorage.getItem(key, callback);
+  },
+
+  /**
+   * Sets value for key and calls callback on completion, along with an Error if there is any
+   */
+  setItem: async (key: string, value: string, callback?: (error?: Error) => void): Promise<void> => {
+    if (!value) {
+      return AsyncStorage.setItem(key, value, callback);
+    }
+
+    if (!shouldInsert) {
+      return AsyncStorage.setItem(key, value, callback);
+    }
+
+    const property = value.slice(0, value.indexOf('\n'));
+
+    //Checking if it's of type Assignments. If yes, it must return.
+    if (Object.prototype.hasOwnProperty.call(JSON.parse(property), 'structure_id')) {
+      return AsyncStorage.setItem(key, value, callback);
+    }
+
+    await AsyncStorage.getItem(currentKey, (error, item) => {
+      if (!error && item && item.length >= 1400000) {
+        currentKey = `${config.APP_NAME}_structures_${structureKey}`;
+        structureKey++;
+
+        void AsyncStorage.setItem(currentKey, value.replace(previous, ''), callback);
+      } else {
+        void AsyncStorage.setItem(currentKey, `${item || ''}${value.replace(previous, '')}`, callback);
+
+        if (structureKey === 1) {
+          previous = value;
+        }
+      }
+    });
+
+    return Promise.resolve();
+  },
+
+  removeItem: async (key: string, callback?: (error?: Error) => void): Promise<void> => {
+    return AsyncStorage.removeItem(key, callback);
+  },
+};
+
+const getData = async (id: number | null, keys: string[], search = false, input: string): Promise<Structure[]> => {
+  try {
+    if (!id && !search) return [];
+
+    let list = '';
+
+    if (keys && keys.length) {
+      const filteredKeys = keys.filter((item) => !item.includes('_assignments'));
+
+      const stores = await AsyncStorage.multiGet(filteredKeys);
+
+      if (stores && stores.length) {
+        stores.forEach((item) => {
+          if (item.length && item.length > 1) {
+            list += item[1];
+          }
+        });
+      }
+    }
+
+    const jsonList = JSON.parse('[' + list.replace(/\n/g, ',') + '{}]') as Structure[];
+
+    let filteredList: Structure[];
+
+    if (search) {
+      let count = 0;
+
+      filteredList = jsonList.filter(function (item) {
+        if (
+          count < 20 &&
+          item.location_path &&
+          item.location_path.toLocaleLowerCase().includes(input.toLocaleLowerCase())
+        ) {
+          count++;
+          return true;
+        }
+        return false;
+      });
+    } else {
+      filteredList = jsonList.filter((item) => item.parent_id === id);
+    }
+
+    filteredList.sort((a, b) => {
+      if (a.display_name > b.display_name) {
+        return 1;
+      }
+      if (a.display_name < b.display_name) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return filteredList;
+  } catch (error) {
+    logErrorToSentry('[ERROR][RequestStorageDataBase]', {
+      severity: Sentry.Severity.Error,
+      infoMessage: error?.message,
+    });
+
+    return [];
+  }
+};
+
 export function createStructureDb() {
-  const db = new Datastore({ filename: `${config.APP_NAME}_structures`, storage: AsyncStorage });
+  const db = new Datastore({
+    filename: currentKey,
+    storage: asyncMethods,
+  });
 
   return {
     loadPromise: new Promise<void>((resolve, reject) => {
@@ -149,6 +269,8 @@ export function createStructureDb() {
     },
 
     insertPage(page: Structure[]) {
+      shouldInsert = true;
+
       return new Promise<void>((resolve, reject) => {
         db.insert(page, function (err) {
           if (err) {
@@ -161,11 +283,17 @@ export function createStructureDb() {
       });
     },
 
-    search(input: string, field = 'display_name', limit = 20) {
-      return db
-        .find({ [field]: createSearchRegex(input) })
-        .limit(limit)
-        .exec() as Promise<Structure[]>;
+    async search(input: string, field = 'display_name', limit = 20) {
+      const keys = await AsyncStorage.getAllKeys();
+
+      if (keys && keys.length > 2) {
+        return getData(null, keys, true, input);
+      } else {
+        return db
+          .find({ [field]: createSearchRegex(input) })
+          .limit(limit)
+          .exec() as Promise<Structure[]>;
+      }
     },
 
     get(id: number | null) {
@@ -183,14 +311,20 @@ export function createStructureDb() {
         .exec() as Promise<Structure[]>;
     },
 
-    getChildren(id: number | null) {
-      return db.find({ parent_id: id }).sort({ display_name: 1 }).exec() as Promise<Structure[]>;
+    async getChildren(id: number | null) {
+      const keys = await AsyncStorage.getAllKeys();
+
+      if (keys && keys.length > 2) {
+        return getData(id, keys, false, '');
+      } else {
+        return db.find({ parent_id: id }).sort({ display_name: 1 }).exec() as Promise<Structure[]>;
+      }
     },
   };
 }
 
 export function createAssignmentDb() {
-  const db = new Datastore({ filename: `${config.APP_NAME}_assignments`, storage: AsyncStorage });
+  const db = new Datastore({ filename: `${config.APP_NAME}_assignments`, storage: asyncMethods });
 
   return {
     loadPromise: new Promise<void>((resolve, reject) => {
@@ -255,12 +389,13 @@ export function createAssignmentDb() {
   };
 }
 
-export const structuresDb: StructuresDb = !getMockFlags(true).DB ? createStructureDb() : createStructureMock();
 export const assignmentsDb: AssignmentDb = !getMockFlags(true).DB ? createAssignmentDb() : createAssignmentMock();
+export const structuresDb: StructuresDb = !getMockFlags(true).DB ? createStructureDb() : createStructureMock();
 
 export async function cleanMongo() {
   await structuresDb.clean();
   await assignmentsDb.clean();
+  await AsyncStorage.clear();
 }
 
 export async function cleanAsyncStorage() {
